fc977002138b73351ea8059e9ced1dcd40b4a39e
imbieras <96958120+imbieras@users.noreply.github.com>
2022-11-17 00:45:53 +0200

    JS fixed to include index.html (#4)

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,6 @@
-// FIXME: no path(index.html) does not get parsed
-function add_active() {
-    var current = location.pathname.split('/')[1];
-    if (current === "") return;
-    var menu_items = document.querySelectorAll('a.nav-link');
-    for (var i = 0, len = menu_items.length; i < len; i++) {
-        if (menu_items[i].getAttribute("href").indexOf(current) !== -1) {
-            menu_items[i].className += " active";
-        }
+document.querySelectorAll(".nav-link").forEach(link => {
+    if(link.href === window.location.href)
+    {
+        link.setAttribute("aria-current", "page")
     }
-};
-
-document.addEventListener("DOMContentLoaded", function () {
-    add_active();
-});
+})
